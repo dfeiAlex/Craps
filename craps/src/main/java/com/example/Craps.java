4a88f8f4fde536c4c730ae87e8c66217f13b54d2
dfeiAlex <20221937@dfei.ie>
2023-01-18 20:56:16 +0000

    Clean up code, add comments

--- craps/src/main/java/com/example/Craps.java
+++ craps/src/main/java/com/example/Craps.java
@@ -10,37 +10,50 @@ import java.util.Random;
 // Else if the sum is 2, 3 or 12, the player loses
 // Otherwise, the sum becomes the point and the player plays another round
 
+// 2nd+ roll:
+// Player rolls again
+// If the sum equals the point, the player wins
+// If the sum equals 7, the player loses
+// Otherwise, play next round
+
 public class Craps {
+    // Declare object variables
     int dieValue1, dieValue2, point;
     boolean keepRolling;
     Random randInt = new Random();
 
+    // Constructor, set initial variable values
     public Craps() {
         dieValue1 = 0;
         dieValue2 = 0;
         keepRolling = false;
     }
 
+    // Generate random numbers for dice
     public void rollDice() {
+        // Get a random number from 1 to 6 inclusive
         dieValue1 = randInt.nextInt(6) + 1;
         dieValue2 = randInt.nextInt(6) + 1;
     }
 
+    // Play a round of Craps
     public String play() {
+        // Roll dice and get their sum
         rollDice();
         int diceSum = dieValue1 + dieValue2;
 
-        // First round
+        // First round logic
         if (!keepRolling) {
             switch (diceSum) {
-                case 2:
-                case 3:
-                case 12:
-                    return "Loss";
                 case 7:
                 case 11:
                     return "Win";
+                case 2:
+                case 3:
+                case 12:
+                    return "Lose";
                 default:
+                    // Player has neither won nor lost, return to game loop
                     keepRolling = true;
                     point = diceSum;
                     return "Continue";
@@ -56,11 +69,12 @@ public class Craps {
             return "Loss";
         }
 
+        // Player didn't win or lose, return to game loop
         return "Continue";
     }
 
     public static void main(String[] args) {
-        Craps obj = new Craps();
-        System.out.println(obj.play());
+        Craps game = new Craps();
+        System.out.println(game.play());
     }
 }
